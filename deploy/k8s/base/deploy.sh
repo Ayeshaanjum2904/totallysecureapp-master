5e611af21ad17c0132620185e1ed591c47a91a62
Ayeshaanjum2904 <ayeshaanjum2904@gmail.com>
2026-07-08 10:40:17 +0530

    security(repohealth): automatically remediate 2 infrastructure issue(s) in deploy.sh

--- deploy/k8s/base/deploy.sh
+++ deploy/k8s/base/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 cd "$(dirname $0)"
-kubectl create namespace crapi
-#kubectl apply -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
+NAMESPACE=${NAMESPACE:-crapi}
+kubectl create namespace $NAMESPACE
 
-kubectl apply -n crapi -f ./rbac
-kubectl apply -n crapi -f ./mongodb
-kubectl apply -n crapi -f ./postgres
-kubectl apply -n crapi -f ./mailhog
-kubectl apply -n crapi -f ./identity
-kubectl apply -n crapi -f ./community
-kubectl apply -n crapi -f ./workshop
-kubectl apply -n crapi -f ./web
+kubectl apply -n $NAMESPACE -f ./rbac
+kubectl apply -n $NAMESPACE -f ./mongodb
+kubectl apply -n $NAMESPACE -f ./postgres
+kubectl apply -n $NAMESPACE -f ./mailhog
+kubectl apply -n $NAMESPACE -f ./identity
+kubectl apply -n $NAMESPACE -f ./community
+kubectl apply -n $NAMESPACE -f ./workshop
+kubectl apply -n $NAMESPACE -f ./web
